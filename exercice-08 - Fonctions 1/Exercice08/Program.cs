0daa082eacfe8fd08761ebeead5ceade933bb5a2
Pierre Poulin <ppoulin@cegep-ste-foy.qc.ca>
2023-01-18 10:19:21 -0500

    Initial commit

---
+++ exercice-08 - Fonctions 1/Exercice08/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercice08
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      // N'oubliez pas d'appeler vos fonctions ici pour les essayer.
+	  // A COMPLÉTER	
+
+    }
+	// A COMPLÉTER
+
+  }
+}
